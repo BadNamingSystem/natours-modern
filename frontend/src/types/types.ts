82768a784bafbc4b1c686f1aeb690cac460b87a3
BadNamingSystem <vlad.claici@yahoo.com>
2026-03-13 19:46:16 +0200

    connect backend, home page, tour details hero

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,17 +1,43 @@
-export type TestTour = {
-    id: number
+export type Tour = {
+    id: string
     name: string
-    duration: number
-    maxGroupSize: number
+    slug: string
+    createdAt: string
+    summary: string
+    description: string | null
     difficulty: string
+    duration: number
+    imageCover: string
+    images: string[]
+    maxGroupSize: number
     ratingsAverage: number
     ratingsQuantity: number
     price: number
-    summary: string
-    startLocation: string
+    priceDiscount: number | null
+    secretTour: boolean
     stops: number
-    description: string
-    imageCover: string
-    images: string[]
     startDates: string[]
+    startLocation: {
+        type: string
+        address: string
+        coordinates: [number, number]
+        description: string
+    }
+    locations: {
+        id: string
+        type: string
+        coordinates: [number, number]
+        description: string
+    }[]
+    guides?: {
+        id: string
+        name: string
+        role: string
+        photo: string
+    }[]
+    reviews?: {
+        id: string
+        review: string
+        rating: number
+    }
 }
